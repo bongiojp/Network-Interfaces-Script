9f5753e53daea7dcdb9bd84f03c6d9556666ceed
Mike Koetter <mike.koetter@gmail.com>
2017-09-22 15:01:52 -0700

    update awk scripts, add --output all on read

--- src/script-runner.js
+++ src/script-runner.js
@@ -1,48 +1,55 @@
 'use strict';
 
-const exec = require('child_process').exec;
-const path     = require('path');
+var exec = require('child_process').exec;
+var path = require('path');
 
-const READ_SCRIPT_PATH  = path.join(__dirname, './awk/readInterfaces.awk');
-const WRITE_SCRIPT_PATH = path.join(__dirname, './awk/changeInterface.awk');
-const DEFAULT_INTERFACE = 'eth0';
-const DEFAULT_INTERFACE_FILE_LOCATION = '/etc/network/interfaces';
-const ADDRESS_RETURN_ORDER = ['address', 'netmask', 'gateway'];
+var READ_SCRIPT_PATH = path.join(__dirname, './awk/readInterfaces.awk');
+var WRITE_SCRIPT_PATH = path.join(__dirname, './awk/changeInterface.awk');
+var DEFAULT_INTERFACE = 'eth0';
+var DEFAULT_INTERFACE_FILE_LOCATION = '/etc/network/interfaces';
+var ADDRESS_RETURN_ORDER = ['address', 'netmask', 'gateway'];
 
+var convertArgsForScript = function convertArgsForScript(interfacesFilePath, args) {
+  var converted = [interfacesFilePath];
 
-const convertArgsForScript = function convertArgsForScript (interfacesFilePath, args) {
-  const converted = [interfacesFilePath];
-
-  // The script requires that device be specified as 
+  // The script requires that device be specified as
   // "dev=<deviceName>" if changing the interface
-  converted.push(`dev=${args.device}`);
+  converted.push('dev=' + args.device);
   delete args.device;
 
-  for (let key in args) {
-    converted.push(`${key}=${args[key]}`);
+  for (var key in args) {
+    converted.push(key + '=' + args[key]);
   }
 
   return converted.join(' ');
-}
+};
 
-const formatResult = function formatResult (textResult) {
-  const rawResult  = textResult.trim().split(' ');
-  const formattedResult = {};
+var formatResult = function formatResult(textResult) {
+  console.log(textResult)
+  var rows = textResult.replace(/^(?=\n)$|^\s*|\s*$|\n\n+/gm,"").split('\n');
+  var rawResult = rows[0].split(' ');
+  var formattedResult = {};
 
   if (rawResult[0] === 'dhcp' || rawResult[0] === 'manual') {
     formattedResult.mode = rawResult[0];
   } else {
-    for (let i = 0; i < rawResult.length; i++) {
+    for (var i = 0; i < rawResult.length; i++) {
       formattedResult[ADDRESS_RETURN_ORDER[i]] = rawResult[i];
+    }
+    if(rows.length > 1){
+      for(var n = 1;n < rows.length; n++){
+        var fields = rows[n].split(' ');
+        formattedResult[fields[0]] = fields[1] || '';
+      }
     }
   }
 
   return formattedResult;
 };
 
-const writeToFile = function writeToFile(filePath, content) {
-  return new Promise((resolve, reject) => {
-    exec(`echo "${content}" | sudo tee  ${filePath} > /dev/null`, (error, stdout, stderr) => {
+var writeToFile = function writeToFile(filePath, content) {
+  return new Promise(function (resolve, reject) {
+    exec('echo "' + content + '" | sudo tee  ' + filePath + ' > /dev/null', function (error, stdout, stderr) {
       if (error) {
         reject(error);
       } else {
@@ -52,57 +59,52 @@ const writeToFile = function writeToFile(filePath, content) {
   });
 };
 
-const runScript = function runScript (scriptName, args) {
-	return new Promise((resolve, reject) => {
-		const child = exec(`awk -f ${scriptName} ${args}`, (error, stdout, stderr) => {
-		  if (error) {
-		    reject(error);
-		  }
+var runScript = function runScript(scriptName, args) {
+  return new Promise(function (resolve, reject) {
+    var child = exec('awk -f ' + scriptName + ' ' + args, function (error, stdout, stderr) {
+      if (error) {
+        reject(error);
+      }
       resolve(stdout);
-		});
-	});
-};
-
-const read = function read (interfacesFilePath, interfaceName) {
-  interfacesFilePath = interfacesFilePath || DEFAULT_INTERFACE_FILE_LOCATION;
-  interfaceName = interfaceName || DEFAULT_INTERFACE;
-
-  return new Promise((resolve, reject) => {
-    runScript(READ_SCRIPT_PATH, [interfacesFilePath, `device=${interfaceName}`].join(' '))
-      .then((success) => {
-         resolve(formatResult(success));
-      }).catch((error) => {
-        reject(error);
-      })
+    });
   });
-  
 };
 
-const write = function write (interfacesFilePath, interfaceName, args) {
+var read = function read(interfacesFilePath, interfaceName) {
   interfacesFilePath = interfacesFilePath || DEFAULT_INTERFACE_FILE_LOCATION;
   interfaceName = interfaceName || DEFAULT_INTERFACE;
 
-  return new Promise ((resolve, reject) => {
-    args.device = interfaceName;
-    const formattedArgs = convertArgsForScript(interfacesFilePath, args);
+  return new Promise(function (resolve, reject) {
+    runScript(READ_SCRIPT_PATH, [interfacesFilePath, 'device=' + interfaceName,'output=all'].join(' ')).then(function (success) {
+      resolve(formatResult(success));
+    }).catch(function (error) {
+      reject(error);
+    });
+  });
+};
 
-    runScript(WRITE_SCRIPT_PATH, formattedArgs)
-      .then((newText) => {
-        writeToFile(interfacesFilePath, newText).then((success) => {
-          resolve(args);
-        })
-        .catch((fileWriteError) => {
-          reject(fileWriteError);
-        })
-      })
-      .catch((error) => {
-        console.log(`Error running script: ${error}`);
-        reject(error);
+var write = function write(interfacesFilePath, interfaceName, args) {
+  interfacesFilePath = interfacesFilePath || DEFAULT_INTERFACE_FILE_LOCATION;
+  interfaceName = interfaceName || DEFAULT_INTERFACE;
+
+  return new Promise(function (resolve, reject) {
+    args.device = interfaceName;
+    var formattedArgs = convertArgsForScript(interfacesFilePath, args);
+
+    runScript(WRITE_SCRIPT_PATH, formattedArgs).then(function (newText) {
+      writeToFile(interfacesFilePath, newText).then(function (success) {
+        resolve(args);
+      }).catch(function (fileWriteError) {
+        reject(fileWriteError);
       });
+    }).catch(function (error) {
+      console.log('Error running script: ' + error);
+      reject(error);
+    });
   });
 };
 
 module.exports = {
-	read,
-	write
+  read: read,
+  write: write
 };
